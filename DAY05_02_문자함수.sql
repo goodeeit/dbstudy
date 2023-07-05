b12f4ac18b977a1190f76025cfe9b720a6024e96
goodeeit <gt_min@naver.com>
2023-07-05 12:20:30 +0900

    20230705

--- DAY05_02_문자함수.sql
+++ DAY05_02_문자함수.sql
@@ -29,6 +29,7 @@ SELECT *
 
 -- 5. 일부만 반환하기
 SELECT SUBSTR(PHONE_NUMBER, 1, 3)  -- 전화번호 1번째 글자부터 3글자를 반환
+     , SUBSTR(PHONE_NUMBER, 5)     -- 전화번호 5번째 글자부터 끝까지 반환
   FROM EMPLOYEES;
 
 -- 6. 특정 문자의 위치 반환하기
@@ -62,8 +63,20 @@ SELECT '[' || LTRIM('     HELLO     WORLD     ') || ']'  -- 왼쪽 공백 제거
 -- 실습.
 -- 1. 사원 테이블의 JOB_ID에서 밑줄(_) 앞/뒷 부분 분리 조회하기
 -- 예시) IT_PROG   ->    IT / PROG
+--       글자수   : 7
+--       밑줄위치 : 3
+--       밑줄 앞 글자수 : 2(밑줄위치 - 1)
+--       밑줄 뒤 글자수 : 4(글자수 - 밑줄위치) (사실은 계산이 필요하지 않다.)
+SELECT JOB_ID
+     , SUBSTR(JOB_ID, 1, INSTR(JOB_ID, '_') - 1)
+     , SUBSTR(JOB_ID, INSTR(JOB_ID, '_') + 1, LENGTH(JOB_ID) - INSTR(JOB_ID, '_'))
+     , SUBSTR(JOB_ID, INSTR(JOB_ID, '_') + 1)
+  FROM EMPLOYEES;
 
 -- 2. FIRST_NAME과 LAST_NAME을 연결해서 모두 대문자로 바꾼 FULL_NAME 조회하기
 -- 예시) FIRST_NAME : Steven
 --       LAST_NAME  : King
 --       FULL_NAME  : STEVEN KING
+SELECT UPPER(CONCAT(CONCAT(FIRST_NAME, ' '), LAST_NAME))
+  FROM EMPLOYEES
+ ORDER BY EMPLOYEE_ID;
