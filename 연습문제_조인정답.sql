98db4da21005a6163ff6339dbddc054310a555d1
goodeeit <gt_min@naver.com>
2023-07-07 09:48:19 +0900

    20230707

--- 연습문제_조인정답.sql
+++ 연습문제_조인정답.sql
@@ -101,28 +101,33 @@ SELECT D.DEPARTMENT_ID, D.DEPARTMENT_NAME, L.STATE_PROVINCE, C.COUNTRY_NAME, R.R
 
 
 -- 8. 모든 사원들의 EMPLOYEE_ID, FIRST_NAME, LAST_NAME, MANAGER의 FIRST_NAME을 조회하시오. (SELF JOIN)
+-- 1) ANSI
 
--- 1:M 관계 파악
--- PK            FK
--- EMPLOYEE_ID   MANAGER_ID
+-- FROM
+-- EMPLOYEES E : 사원테이블
+-- EMPLOYEES M : 상사테이블
 
--- 조인 조건 파악
--- 사원테이블 E        -  매니저테이블 M
--- 사원들의 매니저번호 -  매니저의 사원번호
+-- ON
+-- 사원테이블의 매니저번호 = 상사테이블의 사원번호
 
- -- 1) ANSI
-SELECT E.EMPLOYEE_ID, E.FIRST_NAME, E.LAST_NAME  -- 각 사원들의 정보
-     , M.FIRST_NAME                              -- 매니저 정보
-  FROM EMPLOYEES E LEFT OUTER JOIN EMPLOYEES M
-    ON E.MANAGER_ID = M.EMPLOYEE_ID
- ORDER BY E.EMPLOYEE_ID;
+-- 사원테이블 E (드리븐테이블)   | 상사테이블 M (드라이브테이블)
+--                   FK          | PK
+-- 사원번호  이름    매니저번호  | 사원번호  이름    매니저번호
+-- 1         KIM     NULL        |  1        KIM     NULL
+-- 2         LEE     1           |  2        LEE     1
+-- 3         PARK    1           |  3        PARK    1
+
+SELECT E.EMPLOYEE_ID, E.FIRST_NAME, E.LAST_NAME  -- 사원 정보
+     , M.EMPLOYEE_ID, M.FIRST_NAME, M.LAST_NAME  -- 상사 정보
+  FROM EMPLOYEES M RIGHT OUTER JOIN EMPLOYEES E
+    ON M.EMPLOYEE_ID = E.MANAGER_ID;
+
 
 -- 2) 오라클
-SELECT E.EMPLOYEE_ID, E.FIRST_NAME, E.LAST_NAME  -- 각 사원들의 정보
-     , M.FIRST_NAME                              -- 매니저 정보
-  FROM EMPLOYEES E, EMPLOYEES M
- WHERE E.MANAGER_ID = M.EMPLOYEE_ID(+)
- ORDER BY E.EMPLOYEE_ID;
+SELECT E.EMPLOYEE_ID, E.FIRST_NAME, E.LAST_NAME  -- 사원 정보
+     , M.EMPLOYEE_ID, M.FIRST_NAME, M.LAST_NAME  -- 상사 정보
+  FROM EMPLOYEES M, EMPLOYEES E
+ WHERE M.EMPLOYEE_ID(+) = E.MANAGER_ID;
 
 
 -- 9. 각 사원 중에서 매니저보다 먼저 입사한 사원을 조회하시오. (SELF JOIN)
